25ed26e1660e8a580d691ea02da84cb6f1f889d2
Rashmi Pranami <rashmit1806@gmail.com>
2026-01-13 15:46:57 +0530

    Add zoom-on-hover and arrow navigation for product image gallery

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Product, Size, Brand
+from .models import Category, Product, Size, Brand, ProductImage
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Size)
 admin.site.register(Brand)
+admin.site.register(ProductImage)
